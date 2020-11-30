2911e2b5d2861670f1e17e93aef6601e8784d8bd
s.dibrova <s.dibrova@finik.pro>
2020-11-30 23:42:32 +0200

    microservices with spring cloud and rabbit

---
+++ limit-services/src/main/java/com/demo/limitservices/LimitsConfigurationController.java
@@ -0,0 +1,28 @@
+package com.demo.limitservices;
+
+import com.demo.limitservices.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+  @Autowired Configuration configuration;
+
+  @GetMapping("/limits")
+  public LimitConfiguration retrieveLimitsFromConfiguration() {
+    return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+  }
+
+  @GetMapping("/fault-tolerance-example")
+  @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+  public LimitConfiguration retrieveConfiguration() {
+    throw new RuntimeException("Not available");
+  }
+
+  public LimitConfiguration fallbackRetrieveConfiguration() {
+    return new LimitConfiguration(999, 9);
+  }
+}
